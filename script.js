9910beedd9d812bd2b6725861a871e05436691e9
madhavan136 <madhavandj72@gmail.com>
2026-08-28 20:29:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loading-screen");
-
-    setTimeout(() => {
-        loader.classList.add("hide");
-    }, 3000); // 3 seconds
-});
